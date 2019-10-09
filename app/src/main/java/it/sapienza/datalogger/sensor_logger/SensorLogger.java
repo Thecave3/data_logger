e31729a5ed2a42045b141a6408c66a0806347d58
Andrea Lacava <thecave003@gmail.com>
2019-10-09 16:27:25 +0200

    stub of SensorLogger

---
+++ app/src/main/java/it/sapienza/datalogger/sensor_logger/SensorLogger.java
@@ -0,0 +1,28 @@
+package it.sapienza.datalogger.sensor_logger;
+
+public class SensorLogger {
+    
+    private String path;
+
+
+    public SensorLogger(String path) {
+
+    }
+
+    public void startRegistration() {
+
+    }
+
+    public void writeLog(String timestamp, String s) {
+
+    }
+
+    public void closeRegistration() {
+
+    }
+
+
+    public void closeLogger() {
+
+    }
+}
